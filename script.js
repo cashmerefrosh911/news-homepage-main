f7e1a669c00e79dd8d0a2e749a5ed9e7276472a7
Robert <63929802+cashmerefrosh911@users.noreply.github.com>
2022-12-14 15:25:06 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const openMenu = document.querySelector("#open-menu");
+const ul = document.querySelector("nav ul");
+const closeMenu = document.querySelector("#close-menu");
+console.log(ul)
+
+openMenu.addEventListener("click", () => {
+ul.classList.toggle("show");
+closeMenu.style.display = "block";
+openMenu.style.display = "none";
+})
+
+closeMenu.addEventListener("click", () => {
+closeMenu.style.display = "none";
+openMenu.style.display = "block";
+ul.classList.toggle("show");
+})
